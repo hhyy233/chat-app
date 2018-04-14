6dfdf9e0948a192d75662e969e662a576115cfc5
hydrogen <wybzdqsmmz@outlook.com>
2018-04-14 13:28:54 -0400

    Init the app

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const express = require('express');
+
+const publicPath = path.join(__dirname, '../public', );
+const port = process.env.PORT || 3000;
+
+// console.log(__dirname + '/../public');
+// console.log(publicPath);
+
+var app = express();
+
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+	console.log(`Server is up on port ${port}`);
+});
+
