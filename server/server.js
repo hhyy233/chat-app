3a569591fe8526e0f42a08c2c8e1c8e844a16943
hydrogen <wybzdqsmmz@outlook.com>
2018-04-14 15:24:15 -0400

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -15,11 +15,29 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
 	console.log('New user connected...');
 	
+	socket.emit('newMessage',{
+		from: 'Admin',
+		text: 'Welcome to the chat app',
+		createdAt: new Date().getTime()
+	});
+	socket.broadcast.emit('newMessage', {
+		from: 'Admin',
+		text: 'New user joined',
+		createdAt: new Date().getTime()
+	});
+	
 	socket.on('disconnect', () => {
 		console.log('User was disconnected...')
 	});
 	
 	socket.on('createMessage', (message) => {
+		
+		// socket.broadcast.emit('newMessage', {
+			// from: message.from,
+			// text: message.text,
+			// createdAt: new Date().getTime()
+		// });
+		
 		io.emit('newMessage', {
 			from: message.from,
 			text: message.text,
