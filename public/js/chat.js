1359667a05e8b768424289cb8437daf65b3009f3
hydrogen <wybzdqsmmz@outlook.com>
2018-04-14 23:59:21 -0400

    Send messages to only people in same room

--- public/js/chat.js
+++ public/js/chat.js
@@ -95,7 +95,6 @@ jQuery('#message-form').on('submit', function (e) {
 	var messageTextbox = jQuery('[name=message]');
 	
 	socket.emit('createMessage', {
-		from: 'User',
 		text: messageTextbox.val()
 	}, function () {
 		messageTextbox.val(''); // clear the input
--- server/server.js
+++ server/server.js
@@ -46,13 +46,21 @@ io.on('connection', (socket) => {
 	});
 	
 	socket.on('createMessage', (message, callback) => {
-		io.emit('newMessage', generateMessage(message.from, message.text));
-		console.log('createMessage', message);
-		callback();// acknowledge
+		var user = users.getUser(socket.id);
+		if(user && isRealString(message.text)){
+			io.to(user.room).emit('newMessage', generateMessage(user.name, message.text));
+			console.log(`${user.name}: ${user.room}: ${message.text}`);
+			callback();// acknowledge
+		}
+		
 	});
 	
 	socket.on('createLocationMessage', (coords) => {
-		io.emit('newLocationMessage', generateLocationMessage('Admin',coords.latitude, coords.longitude));
+		var user = users.getUser(socket.id);
+		
+		if(user){
+			io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name,coords.latitude, coords.longitude));
+		}
 	});
 	
 	socket.on('disconnect', () => {
